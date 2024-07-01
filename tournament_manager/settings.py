da95ec6c37f71daf7427570aad84fead9f229bd5
ArzMarsel <marselarzybekov3@gmail.com>
2024-07-01 16:32:13 +0600

    assa

--- tournament_manager/settings.py
+++ tournament_manager/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&ve^3)gj8i3c0)cctod))!cdkfsj-3r%hn-s-exfvk-idgxzpg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['tourstore.student-project-it-academy.tw1.ru'
                  'www.tourstore.student-project-it-academy.tw1.ru']
+
 CSRF_TRUSTED_ORIGINS = [
     'https://tourstore.student-project-it-academy.tw1.ru',
     'https://www.tourstore.student-project-it-academy.tw1.ru',
